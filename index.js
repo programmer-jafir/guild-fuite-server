ce90a14bf936e42cb3b08ebf10826cba8bcc75ef
programmer-jafir <jafiranjum2020@gmail.com>
2022-05-08 20:06:16 +0600

    jwt removed

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: 'unauthorized access' });
-  }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: 'Forbidden access' });
-    }
-    console.log('decoded', decoded);
-    req.decoded = decoded;
-    next();
-  })
-}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zykkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -36,46 +21,37 @@ async function run() {
     const itemCollection = client.db('warehouse-management-website-').collection('item');
     const myitemCollection = client.db('warehouse-management-website-').collection('myitem')
 
-
-    // AUTH
-    app.post('/login', async (req, res) => {
-      const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1d'
-      });
-      res.send({ accessToken });
-    })
     // get items
     app.get('/item', async (req, res) => {
       const qurey = {};
       const cursor = itemCollection.find(qurey);
       const items = await cursor.toArray();
-
+      
       res.send(items);
     });
 
-    // app.get('/item', async (req, res) => {
-    //   const qurey = {};
-    //   const cursor = itemCollection.find(qurey);
-    //   const items = await cursor.toArray();
+    app.get('/item', async (req, res) => {
+      // const qurey = {};
+      // const cursor = itemCollection.find(qurey);
+      // const items = await cursor.toArray();
 
-    //   // const email = req.query.email;
-    //   // const query = {email: email};
-    //   // const cursor = myitemCollection.find(query);
-    //   // const items = await cursor.toArray();
+      const email = req.query.email;
+      const query = {email: email};
+      const cursor = myitemCollection.find(query);
+      const items = await cursor.toArray();
+      
+      res.send(items);
+    });
 
-    //   res.send(items);
-    // });
-
-    app.get('/item/:id', async (req, res) => {
+    app.get('/item/:id', async(req, res)=>{
       const id = req.params.id;
-      const qurey = { _id: ObjectId(id) };
+      const qurey = {_id: ObjectId(id)};
       const item = await itemCollection.findOne(qurey);
       res.send(item);
     });
 
     // Post
-    app.post('/item', async (req, res) => {
+    app.post('/item', async(req, res)=>{
       const newItem = req.body;
       const result = await itemCollection.insertOne(newItem);
       res.send(result);
@@ -83,11 +59,11 @@ async function run() {
 
     // UpdateQuantity item
 
-    app.put('/item/:id', async (req, res) => {
+    app.put('/item/:id', async(req, res)=>{
       const id = req.params.id;
       const updateQuantity = req.body;
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
+      const filter = {_id: ObjectId(id)};
+      const options = { upsert: true};
       const updateDocs = {
         $set: {
           quantity: updateQuantity.quantity
@@ -100,36 +76,30 @@ async function run() {
 
     // item Collection API
 
-    app.get('/myitem', verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email;
+    app.get('/myitem', async(req, res) =>{
       const email = req.query.email;
-      if (email === decodedEmail) {
-        const query = { email: email };
-        const cursor = myitemCollection.find(query);
-        const items = await cursor.toArray();
-        res.send(items);
-      }
-      else{
-        res.status(403).send({ message: 'Forbidden access' })
-      }
+      const query = {email: email};
+      const cursor = myitemCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
     })
-
-    app.post('/myitem', async (req, res) => {
+    
+    app.post('/myitem', async(req, res) =>{
       const myitem = req.body;
       const result = await myitemCollection.insertOne(myitem);
       res.send(result);
     })
 
     // Delete
-    app.delete('/item/:id', async (req, res) => {
+    app.delete('/item/:id', async(req, res) =>{
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = {_id: ObjectId(id)};
       const result = await itemCollection.deleteOne(query);
       res.send(result);
 
-      app.delete('/myitem/:id', async (req, res) => {
+      app.delete('/myitem/:id', async(req, res)=>{
         const id = req.params.id;
-        const query = { _id: ObjectId(id) };
+        const query = {_id: ObjectId(id)};
         const result = await myitemCollection.deleteOne(query);
         res.send(result);
       })
